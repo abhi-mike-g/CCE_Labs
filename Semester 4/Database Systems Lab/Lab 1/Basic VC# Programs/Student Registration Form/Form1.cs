59a805f0502fab4ecfc71844ec36eb87aa52b099
AMG <abhimg20@gmail.com>
2025-02-12 11:56:12 +0530

    Add files via upload

---
+++ Semester 4/Database Systems Lab/Lab 1/Basic VC# Programs/Student Registration Form/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Registration_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            // Do Not add any codes here, accidentally clicked on this Label. Please Ignore
+            // Do Not remove
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
