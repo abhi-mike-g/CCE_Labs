129f00f93779d7843e9921fbcbb91417d6e75c33
AMG <abhimg20@gmail.com>
2025-03-11 04:51:54 +0530

    Add files via upload

---
+++ Semester 3/Object Oriented Programming Lab/Lab Week 1/Hello World/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World!");
+		System.out.println("Welcome to ");
+		System.out.print("Object Oriented ");
+		System.out.print("Programming in JAVA");
+	}
+}
