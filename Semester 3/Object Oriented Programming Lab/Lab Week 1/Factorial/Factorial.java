9662bd8b8e61d4c585a8ca8703dc6be406536ce8
AMG <abhimg20@gmail.com>
2025-03-11 04:40:59 +0530

    Add files via upload

---
+++ Semester 3/Object Oriented Programming Lab/Lab Week 1/Factorial/Factorial.java
@@ -0,0 +1,11 @@
+class Factorial
+{
+    public static void main(String args[])
+    {
+        int num, fact = 1, i;
+        num = Integer.parseInt(args[0]);
+        for(i = 1; i <= num; i++)
+            fact = fact*i;
+        System.out.println("Factorial of "+num+" is "+fact);
+    }
+}
