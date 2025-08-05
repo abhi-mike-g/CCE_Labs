aeb9c9b14b781785346904f1d1de5c5478a4a221
AMG <abhimg20@gmail.com>
2025-08-05 08:55:06 +0530

    Update Array_Exp3.s

--- Semester 5/Embedded Systems Design Lab/Lab3/Exp 3.1/Array_Exp3.s
+++ Semester 5/Embedded Systems Design Lab/Lab3/Exp 3.1/Array_Exp3.s
@@ -1,31 +1,25 @@
         AREA RESET, DATA, READONLY
         EXPORT __Vectors
 __Vectors
-		DCD 0X10001000
+	DCD 0X10001000
         DCD Reset_Handler
         ALIGN
-
         AREA mycide, CODE, READONLY
         ENTRY
         EXPORT Reset_Handler
-
 Reset_Handler
         LDR     R0, =SRC
         ADD     R1, R0, #36
         MOV     R4, #5
-
 UP
         LDR     R2, [R0]
         LDR     R3, [R1]
         STR     R3, [R0]
         STR     R2, [R1]
-
         ADD     R0, R0, #4
         SUB     R1, R1, #4
-
         SUBS    R4, R4, #1
         BNE     UP
-
 STOP	B       STOP
 
         AREA READDATA, DATA, READWRITE
