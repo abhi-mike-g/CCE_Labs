2a744e27a5e6a30864be693f1b27c5f18b120edb
AMG <abhimg20@gmail.com>
2025-08-07 11:26:00 +0530

    Create Q4.py

---
+++ Semester 5/Information Security Lab/Lab 1/Q4.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def preprocess(text):
+    text = text.lower().replace(" ", "").replace("j", "i")
+    if len(text) % 2 != 0:
+        text += 'x'
+    return text
+
+def hill_encrypt(text, key_matrix):
+    text = preprocess(text)
+    encrypted = ''
+    for i in range(0, len(text), 2):
+        pair = [ord(text[i]) - ord('a'), ord(text[i+1]) - ord('a')]
+        res = np.dot(key_matrix, pair) % 26
+        encrypted += chr(res[0] + ord('a')) + chr(res[1] + ord('a'))
+    return encrypted
+
+def main():
+    message = "We live in an insecure world"
+    key_matrix = np.array([[3, 3], [2, 7]])
+    cipher = hill_encrypt(message, key_matrix)
+    print("Hill Cipher → Encrypted:", cipher)
+
+if __name__ == "__main__":
+    main()
