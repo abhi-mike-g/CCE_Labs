1aebe298b3e9803fe445ab3f85cf6ca97e478681
AMG <abhimg20@gmail.com>
2025-09-02 10:33:18 +0530

    Add files via upload

---
+++ Semester 5/Embedded Systems Design Lab/Lab6/Baby Blinker/Intro_to_C.c
@@ -0,0 +1,12 @@
+#include <LPC17xx.h>
+
+int main() {
+
+    LPC_PINCON->PINSEL0 &= 0xFF0000FF;
+    LPC_GPIO0->FIODIR |= 0x00000FF0;
+
+    while (1) {
+        LPC_GPIO0->FIOSET = 0x00000FF0;
+        LPC_GPIO0->FIOCLR = 0x00000FF0;
+    }
+}
---
+++ Semester 5/Embedded Systems Design Lab/Lab6/Baby Blinker/intro_to_c.d
@@ -0,0 +1,7 @@
+intro_to_c.o: Intro_to_C.c
+intro_to_c.o: C:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.h
+intro_to_c.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+intro_to_c.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+intro_to_c.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+intro_to_c.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+intro_to_c.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
