8eb2cf94e6b3d16270cf2856989ccc8d00a01f0d
AMG <abhimg20@gmail.com>
2025-10-07 10:41:20 +0530

    Add files via upload

---
+++ Semester 5/Embedded Systems Design Lab/LabX/Q1/ledwap.d
@@ -0,0 +1,8 @@
+ledwap.o: LEDWAP.c
+ledwap.o: C:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.h
+ledwap.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+ledwap.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+ledwap.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+ledwap.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+ledwap.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
+ledwap.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
