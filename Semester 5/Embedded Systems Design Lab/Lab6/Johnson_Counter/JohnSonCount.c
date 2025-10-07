ab88281db91b7d75f9a914073c483c0bd1dd26c9
AMG <abhimg20@gmail.com>
2025-10-07 06:04:31 +0530

    Update JohnSonCount.c

--- Semester 5/Embedded Systems Design Lab/Lab6/Johnson_Counter/JohnSonCount.c
+++ Semester 5/Embedded Systems Design Lab/Lab6/Johnson_Counter/JohnSonCount.c
@@ -1,21 +1,30 @@
 #include <LPC17xx.h>
 
+void delay(void) {
+    int i;
+    for (i = 0; i < 500000; i++);
+}
+
 int main(void) {
+
     LPC_PINCON->PINSEL2 &= 0x3FFFFFFF;
     LPC_PINCON->PINSEL3 &= 0xFFFF0000;
+
     LPC_GPIO1->FIODIR |= 0x00FF8000;
 
-    while (1) {
-        unsigned int x = 0x8000;
-        for (int i = 0; i < 9; i++) {
-            LPC_GPIO1->FIOSET = x;
-            x = x << 1;
-        }
+    unsigned char pattern = 0x00; 
+    int i;
 
-        x = 0x8000;
-        for (int i = 0; i < 9; i++) {
-            LPC_GPIO1->FIOCLR = x;
-            x = x << 1;
+    while (1) {
+        for (i = 0; i < 16; i++) { 
+            LPC_GPIO1->FIOPIN = (pattern << 15);
+
+            delay();
+            
+            unsigned char feedback = (~(pattern >> 7)) & 0x01;
+            pattern = (pattern << 1) | feedback;
+
+            pattern &= 0xFF;
         }
     }
 }
